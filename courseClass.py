5fcd5384b40054d0316ff6731f8ef0a6a08b4d6d
cep2 <clifporter@gmail.com>
2016-12-07 06:54:46 -0800

    Solidified coruse. Add'l functions, beginning algorithm.

--- courseClass.py
+++ courseClass.py
@@ -5,31 +5,35 @@ Created on Tue Dec  6 23:36:50 2016
 @author: clifp
 """
 
-class Course():
-	def __init__(self, classString = "NOCLASS"):
-		self.name
-		self.units
-		self.startTime
-		self.endTime
-		self.days
-		self.viable = True
+class Course(object):
+	def __init__(self, classString = "NOCLASSINIT"):
+		self.name 			= "NONAME"
+		self.units 			= -1
+		self.startTime 	= "00:00"
+		self.endTime 		= "00:00"
+		self.days 			= "MTWRF"
+		self.viable 		= True
+		self.parseClass(classString)
 		
 	def getName(self): 			return self.name
 	def getUnits(self): 		return self.units
 	def getStartTime(self): 	return self.startTime
 	def getEndTime(self): 		return self.endTime
 	def getDays(self): 			return self.days
+	def isViable(self):		return self.viable
+	
+	
+	
 	
 	def setName(self, inName = "NONAME"):
 		self.name = inName
-		return
-	
+		return	
 	#Input, string of HH:MM, uses class to get 
 	def setStartTime(self, inTime = "00:00"):
 		self.startTime = classTime(inTime)
 		return
-	def setEndTime(self, inTime):
-		self.endTime = classTime(inTime = "00:00")
+	def setEndTime(self, inTime="00:00"):
+		self.endTime = classTime(inTime)
 		return
 	#Input: Char, or int of Units
 	def setUnits(self, unitCount = "-1"):
@@ -39,11 +43,36 @@ class Course():
 	def setDays(self, inDays = "MTWRF"):
 		self.days = inDays
 		return
+	def setViable(self, boolIn):
+		self.viable = boolIn
+		return
+		
+		
+	#Expected Format: Name Units Days Start End	
+	def parseClass(self, classString = "NOCLASSPARSE"):
+		classLine = classString.split()		#Separate by whitespace
+		self.setName(classLine[0])
+		self.setUnits(classLine[1])
+		self.setDays(classLine[2])
+		self.setStartTime(classLine[3])
+		self.setEndTime(classLine[4])
+		return
+		
+	def printCourse(self):
+		print(self.name,end='\t')
+		print(str(self.units),end='\t')
+		print(self.days,end='\t')
+		self.startTime.printClassTime()
+		self.endTime.printClassTime()
+		print()
+		
+		
+		
 #End Course Class
 #================================================
 	
 #Easy creation of time.	
-class classTime():
+class classTime(object):
 	def __init__(self, stringTime = "00:00"):
 		inTime = stringTime.split(':')
 		self.hour = int(inTime[0])
@@ -52,9 +81,50 @@ class classTime():
 	
 	def getHour(self): return self.hour
 	def getMinute(self): return self.minute
+		
+	def printClassTime(self):
+		if (self.hour < 10):
+			strHour = "0"+str(self.hour)
+		else:
+			strHour = str(self.hour)
+		if (self.minute < 10):
+			strMinute = "0"+str(self.minute)
+		else:
+			strMinute = str(self.minute)
+		print("{}:{}".format(strHour, strMinute), end='\t')
 #End Time Class
 #================================================	
+
+def isCompatibleCourse(courseA=None, courseB=None):
+	if (isCompatibleDay(courseA, courseB) == False):
+		if (isCompatibleTime(courseA,courseB) == False):
+			return False
+	return True
+#End isCompatibleCourse
+#================================================
+
+def isCompatibleDay(courseA=None, courseB=None):
+	answer = True
+	#Check for present classes
+	if (courseA == None):
+		print("NO FIRST COURSE")
+		answer = False
+	if (courseB == None):
+		print ("NO SECOND COURSE")
+		answer = False
+	if (answer == False):
+		return answer
+		
+	daysA = courseA.getDays()
+	daysB = courseB.getDays()
 	
+	for day in daysA:
+		if (day in daysB):
+			return False
+	return True
+
+#End isCompatibleDay
+#================================================	
 	
 """
 Compares the two courses to see if they are overlapping
@@ -71,22 +141,23 @@ def isCompatibleTime(courseA=None, courseB=None):
 		print ("NO SECOND COURSE")
 		answer = False
 	if (answer == False):
-		return
+		return			
+			
 	#Pull times into organized array.	
 	timeA = [courseA.getStartTime(), courseA.getEndTime]	#Course 1
 	timeB = [courseB.getStartTime(), courseB.getEndTime]	#Course 2
 	
 	#Avoid assumption that course A starts earlier.
 	#Swap B/A if Hour is earlier, if equal check minute
-	startHourDiff = timeB[0].getHour - timeA[0].getHour
+	startHourDiff = timeB[0].getHour() - timeA[0].getHour()
 	if ( startHourDiff < 0):
 		#Perform a swap if class B starts earlier based on Hour.
 		temp = timeA
 		timeA = timeB
 		timeB = temp
 	#If the hour is equal, check for minutes being less. 
-	elif ( (timeB[0].getHour - timeA[0].getHour) == 0):
-		startMinDiff = timeB[0].getMinute - timeA[0].getMinute
+	elif ( (timeB[0].getHour() - timeA[0].getHour()) == 0):
+		startMinDiff = timeB[0].getMinute() - timeA[0].getMinute()
 		#If both values are 0, then they are the same start time.
 		if (startMinDiff == 0):
 			return False
--- scheduler.py
+++ scheduler.py
@@ -3,16 +3,109 @@
 Created on Tue Dec  6 23:33:08 2016
 
 Final project intended to be able to schedule courses for students given a list of classes.
-Initial prioritization is by unit count.
+Initial prioritization is by listing order.
 
 @author: clifp
 """
 
+#from courseClass import*
+from courseClass import*
 
+BARRIER_TEXT = "===================="
+"""
+Opens file for reading.
 
+"""
+def readClasses(filename="classes.txt"):
+	file = open(filename, "r")
+	courseList = list()
+	for courseLine in file:
+		courseLine = (courseLine.upper())	
+		courseList.append(courseLine)
+	return courseList
+	
+def printCourseList(courseList=None):
+	if (courseList==None):
+		print("No course list to print.")
+		return
+	for listing in courseList:
+		listing.printCourse()
+	
+def countViable(courseList=None):
+	if courseList == None:
+		print ("countViable:\t No list present.")
+		return -1
+	count = 0
+	return count
 
-def main():
+"""
+	Input
+		Current Course List
+	Output
+		Index of next viable course.
+"""
+def findNextViableCourse(courseList=None):
+	if(courseList==None):
+		print("findNextViableCourse():\t Course List is missing.")
+		return
+	#Sort through list to find first viable course
+	index = 0
+	listLen = len(courseList)
+	while index < listLen:
+		if (courseList[index].isViable)
+		index += 1 
+		
+		
+	
+	return -1
+
+"""
+Input
+	Accepted Course List (State)
+	Full Course List (Entire List)
+Objective
+	Take the course at the front of the list (if viable)
+	Compare it to the accepted list.
+		If it fits, add to accepted AND mark all remaining as viable/not
+"""
+def considerCourse(courseList=None, acceptedList=None):
+	if (courseList == None or acceptedList == None):
+		print("considerCourse():\tCourse or Accepted List is missing.")
+		return
+	
 	return
 	
 	
-	main()
+	
+
+def main():
+	classesTextList = readClasses()
+	print(classesTextList)
+	
+	courseList = list()		 	#General Course list
+	acceptedList = list()		#Accepted courses
+	#Add all courses to standard list in structure.
+	for course in classesTextList:
+		newCourse = Course(course)
+		courseList.append(newCourse)
+		
+	
+	#Begin considering courses.
+	
+		
+	#TEST COMPARISONS
+	if ( isCompatibleCourse (courseList[1], courseList[3]) ):
+		print("CLEAR")
+	print("\nCourse List\n" + BARRIER_TEXT)
+	printCourseList(courseList)
+	print(BARRIER_TEXT)
+		
+	
+	
+	
+	
+	
+	return
+	
+	
+main()
