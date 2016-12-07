d59ce49beb79f2f22851d04c7ebaa1b0ffb02db6
cep2 <clifporter@gmail.com>
2016-12-07 01:06:11 -0800

    Basic Class, Basic Time

--- course.py
+++ course.py
@@ -5,3 +5,32 @@ Created on Tue Dec  6 23:36:50 2016
 @author: clifp
 """
 
+class Course():
+	def __init__(self):
+		self.name
+		self.units
+		self.startTime
+		self.endTime
+		self.days
+		
+	def getName(self): 			return self.name
+	def getUnits(self): 		return self.units
+	def getStartTime(self): 	return self.startTime
+	def getEndTime(self): 		return self.endTime
+	def getDays(self): 			return self.days
+	
+	#Input, string of HH:MM
+	def setStartTime(self, inTime):
+		self.startTime = classTime(inTime)
+		return
+	
+#Easy creation of time.	
+class classTime():
+	def __init__(self, stringTime = "00:00"):
+		inTime = stringTime.split(':')
+		self.hour = int(inTime[0])
+		self.minute = int(inTime[1])
+		return
+	
+#End Course Class
+#================================================
--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Dec  6 23:36:24 2016
+Created on Tue Dec  6 23:33:08 2016
+
+Final project intended to be able to schedule courses for students given a list of classes.
+Initial prioritization is by unit count.
 
 @author: clifp
 """
 
+
+
+
+def main():
+	return
+	
+	
+	main()
