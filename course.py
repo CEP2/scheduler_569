9a9e4d2a9dbd5849122464393c6e8ed1a8520bc6
cep2 <clifporter@gmail.com>
2016-12-07 01:30:55 -0800

    classes.txt creation

--- course.py
+++ course.py
@@ -6,12 +6,13 @@ Created on Tue Dec  6 23:36:50 2016
 """
 
 class Course():
-	def __init__(self):
+	def __init__(self, classString = "NOCLASS"):
 		self.name
 		self.units
 		self.startTime
 		self.endTime
 		self.days
+		self.viable = True
 		
 	def getName(self): 			return self.name
 	def getUnits(self): 		return self.units
@@ -19,10 +20,27 @@ class Course():
 	def getEndTime(self): 		return self.endTime
 	def getDays(self): 			return self.days
 	
-	#Input, string of HH:MM
-	def setStartTime(self, inTime):
+	def setName(self, inName = "NONAME"):
+		self.name = inName
+		return
+	
+	#Input, string of HH:MM, uses class to get 
+	def setStartTime(self, inTime = "00:00"):
 		self.startTime = classTime(inTime)
 		return
+	def setEndTime(self, inTime):
+		self.endTime = classTime(inTime = "00:00")
+		return
+	#Input: Char, or int of Units
+	def setUnits(self, unitCount = "-1"):
+		self.units = int(unitCount)
+		return
+	#Input: String of letters, to set (i.e. MWF)
+	def setDays(self, inDays = "MTWRF"):
+		self.days = inDays
+		return
+#End Course Class
+#================================================
 	
 #Easy creation of time.	
 class classTime():
@@ -32,5 +50,25 @@ class classTime():
 		self.minute = int(inTime[1])
 		return
 	
-#End Course Class
-#================================================
+#End Time Class
+#================================================	
+	
+	
+"""
+Compares the two courses to see if they are overlapping
+"""
+def isCompatibleTime(courseA=None, courseB=None):
+	answer = True
+	if (courseA == None):
+		print("NO FIRST COURSE")
+		answer = False
+	if (courseB == None):
+		print ("NO SECOND COURSE")
+		answer = False
+	if (answer == False):
+		return
+		
+	timeA = [courseA.getStartTime(), courseA.getEndTime]
+	timeB = [courseB.getStartTime(), courseB.getEndTime]
+		
+	difference = 
